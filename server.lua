7bc75bb058eae286e1e926ce2ba10117f8bd034a
BackrndSource <back.rndsource@gmail.com>
2019-03-27 20:22:23 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,13 +7,13 @@ AddEventHandler('esx_slotmachine:sv:1', function(amount,a,b,c)
 	local source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
 	amount = tonumber(amount)
-		if (xPlayer.getMoney() >= tonumber(amount)) or (xPlayer.getAccounts('black_money') >= tonumber(amount)) then
+	if (xPlayer.getAccount('black_money').money >= tonumber(amount)) then
+		xPlayer.removeAccountMoney('black_money', (amount))
+		TriggerClientEvent("esx_slotmachine:1", source, tonumber(amount), tostring(a), tostring(b), tostring(c))
+	elseif (xPlayer.getMoney() >= tonumber(amount)) then
 		xPlayer.removeMoney(amount)
-		-- xPlayer.removeAccountMoney('black_money', (amount))
 		TriggerClientEvent("esx_slotmachine:1", source, tonumber(amount), tostring(a), tostring(b), tostring(c))
 	else
-		TriggerClientEvent('esx:showNotification', source, "Not enough money in your wallet")
-	end
 end)
 
 RegisterServerEvent('esx_slotmachine:sv:2')
